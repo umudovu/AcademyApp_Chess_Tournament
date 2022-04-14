e6abbaabbe5169db0aaf6a11a142e24cd2da760b
Umudov Umud <umudau@code.edu.az>
2022-04-15 00:18:49 +0400

    feat: Repository

---
+++ DataAccess/Repository/PlayerRepository.cs
@@ -0,0 +1,82 @@
+﻿using DataAccess.Interfaces;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+
+namespace DataAccess.Repository
+{
+    public class PlayerRepository : IRepository<Player>
+    {
+        public bool Create(Player entity)
+        {
+            try
+            {
+                DataContext.Players.Add(entity);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public bool Update(Player entity)
+        {
+            try
+            {
+                Player IsExsist = GetOne(p => p.Id== entity.Id);
+                IsExsist=entity;
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool Delete(Player entity)
+        {
+            try
+            {
+                DataContext.Players.Remove(entity);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public List<Player> GetAll(Predicate<Player> filter = null)
+        {
+            try
+            {
+                return filter==null ? DataContext.Players :
+                DataContext.Players.FindAll(filter);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public Player GetOne(Predicate<Player> filter = null)
+        {
+            try
+            {
+                return filter==null ? DataContext.Players[0] :
+                DataContext.Players.Find(filter);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+
+    }
+}
---
+++ DataAccess/Repository/TournamentRepositiry.cs
@@ -0,0 +1,38 @@
+﻿using DataAccess.Interfaces;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class TournamentRepositiry : IRepository<Tournament>
+    {
+        public bool Create(Tournament entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Tournament entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Tournament> GetAll(Predicate<Tournament> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Tournament GetOne(Predicate<Tournament> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Tournament entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
